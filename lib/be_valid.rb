442df33d3fd144969edaff83062746ee5ff62a65
John Stewart (johnsinco) <john.stewart@experian.com>
2021-04-26 08:03:28 -0500

    fix typo

--- lib/be_valid.rb
+++ lib/be_valid.rb
@@ -4,6 +4,6 @@ begin
   require 'validators/date_validator'
   require 'validators/must_be_validator'
 rescue 
-  'unable to loan be_valid validator'
+  'unable to load be_valid validator'
   nil
 end
